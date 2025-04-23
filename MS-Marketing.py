641efc05064ee0e37e0223dfd03cadec5c88bfd4
Gwaean <56048874+Gwaean@users.noreply.github.com>
2025-04-23 17:24:23 -0300

    implementando-marketing-itinerarios

--- MS-Marketing.py
+++ MS-Marketing.py
@@ -1,18 +1,56 @@
 import pika
-import sys
+import csv
+import time
+import random
+
+ARQUIVO_CSV = 'itinerários_csv'
 # Marketing envia promocoes do(s) destino(s) selecionado(s) pelo cliente p/ receber notificação quando o preço varia
 # Ele envia SOMENTE para quem escolheu receber E SOMENTE p/ o(s) destino(s) escolhido(s) --> NAO PODE DAR FANOUT
+def ler_precos_csv():
+    precos = {}
+    with open(ARQUIVO_CSV, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            precos[row['Destino']] = int(row['Valor_Pacote'])
+    return precos
+
+def salvar_precos_csv(precos):
+    with open(ARQUIVO_CSV, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Destino', 'Valor_Pacote'])  # cabeçalho
+        for destino, preco in precos.items():
+            writer.writerow({'Destino': destino, 'Valor_Pacote': preco})
 
 # Emite log
+def enviar_notificacao(destino, preco_antigo, preco_novo):
+ connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+ channel = connection.channel()
+ channel.exchange_declare(exchange="promocoes", exchange_type="direct")
+ if preco_novo < preco_antigo:
+        variacao = preco_antigo - preco_novo
+        mensagem = f"🔥 Aproveite! Baixa de preço para {destino}! Agora R${preco_novo} (↓ R${variacao})"
+ else:
+        variacao = preco_novo - preco_antigo
+        mensagem = f"📈 Aumento de preço em {destino}: Agora R${preco_novo} (↑ R${variacao})"
+ 
+ channel.basic_publish(exchange="promocoes", routing_key=destino, body=mensagem)
+ print(f" [x] Sent {destino}:{mensagem}")
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-channel = connection.channel()
-severity = sys.argv[1] if len(sys.argv) > 2 else "info"
-channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
+ connection.close()
+def simular_mudanca_preco():
+    while True:
+        precos = ler_precos_csv()
+        destino = random.choice(list(precos.keys()))
+        preco_antigo = precos[destino]
+        mudanca = random.randint(-300, 300)
+        preco_novo = max(500, preco_antigo + mudanca)
 
-message = " ".join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange="direct_logs", routing_key=severity, body=message)
-print(f" [x] Sent {severity}:{message}")
+        if preco_novo != preco_antigo:
+            enviar_notificacao(destino, preco_antigo, preco_novo)
+            precos[destino] = preco_novo
+            salvar_precos_csv(precos)
 
+        time.sleep(random.randint(5, 10))
 
-connection.close()
+if __name__ == "__main__":
+    simular_mudanca_preco()
